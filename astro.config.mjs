f484f6be3c9c4d870da431d71ad068da43d971b9
Farouq Aldori <farouqaldori@hotmail.com>
2025-12-08 19:50:51 +0100

    Improve mobile layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,9 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://claudeisland.com',
+  vite: {
+    server: {
+      allowedHosts: ['localhost', '.ngrok.app', '.ngrok-free.app'],
+    },
+  },
 });
